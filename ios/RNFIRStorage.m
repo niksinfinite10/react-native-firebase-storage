d5b31fc7a6c476b1977aeac1411c3ca0acef9770
Vani-gurnani <sunny.vanita@gmail.com>
2016-10-14 23:40:44 -0700

    changed to use file

--- ios/RNFIRStorage.m
+++ ios/RNFIRStorage.m
@@ -32,54 +32,37 @@ RCT_REMAP_METHOD(uploadFileToFirebase,
                  : (NSString*) key
                  resolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject){
+    
+    
     NSURL *url = [[NSURL alloc] initWithString:localFile];
     
-    ALAssetsLibrary *assetsLibrary = [[ALAssetsLibrary alloc] init];
-    [assetsLibrary assetForURL:url resultBlock: ^(ALAsset *asset){
-        ALAssetRepresentation *representation = [asset defaultRepresentation];
-        CGImageRef imageRef = [representation fullScreenImage];
-        if (imageRef) {
-            UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
-            imageView.image = [UIImage imageWithCGImage:imageRef scale:representation.scale orientation:representation.orientation];
-            
-            
-            NSData *imageData =  UIImageJPEGRepresentation (imageView.image, 80);
-            FIRStorage *storage = [FIRStorage storage];
-            FIRStorageReference *storageRef = [storage referenceForURL:bucket];
-          
-            FIRStorageMetadata *metadata = [[FIRStorageMetadata alloc] init];
-            metadata.contentType = contentType;
-            
-             FIRStorageUploadTask *uploadTask = [[storageRef child:key]
-             putData:imageData metadata:metadata
-             completion:^(FIRStorageMetadata *metadata, NSError *error) {
-                 [self.bridge.eventDispatcher sendAppEventWithName:@"FirebaseUploadProgressChanged" body: @{ @"progress": [NSNumber numberWithInteger:1], @"key": key}];
-                 if (error) {
-                     NSLog(@"Error uploading: %@", error);
-                     reject(@"Error", @"Failed upload file to firebase", error);
-                     return;
-                 }
-                 resolve(@ {@"success": @"123"});
-                 
-             }];
-             
-               FIRStorageHandle observer = [uploadTask observeStatus:FIRStorageTaskStatusProgress
-            handler:^(FIRStorageTaskSnapshot *snapshot) {
-            
-              double progress =  ((double)snapshot.progress.completedUnitCount / (double)snapshot.progress.totalUnitCount);
-              [self.bridge.eventDispatcher sendAppEventWithName:@"FirebaseUploadProgressChanged" body: @{ @"progress": [NSNumber numberWithDouble:progress], @"key": key}];
-              
-          }];
-
-
-            // ...
+    
+    FIRStorage *storage = [FIRStorage storage];
+    FIRStorageReference *storageRef = [storage referenceForURL:bucket];
+    
+    FIRStorageReference *uploadRef = [storageRef child:key];
+    
+    FIRStorageMetadata *metadata = [[FIRStorageMetadata alloc] init];
+    metadata.contentType = contentType;
+    
+    FIRStorageUploadTask *uploadTask = [uploadRef putFile:url metadata:metadata completion:^(FIRStorageMetadata *metadata, NSError *error) {
+        [uploadTask removeAllObservers];
+        [self.bridge.eventDispatcher sendAppEventWithName:@"FirebaseUploadProgressChanged" body: @{ @"progress": [NSNumber numberWithInteger:1], @"key": key}];
+        if (error) {
+            NSLog(@"Error uploading: %@", error);
+            reject(@"Error", @"Failed upload file to firebase", error);
         }
-    } failureBlock:^(NSError *error) {
-        NSLog(@"Error Loading File: %@", error);
-        reject(@"Error", @"Failed Open File", error);
+        resolve(@ {@"success": @"123"});
+        
     }];
     
-    
+    FIRStorageHandle observer = [uploadTask observeStatus:FIRStorageTaskStatusProgress
+                                                  handler:^(FIRStorageTaskSnapshot *snapshot) {
+                                                      
+                                                      double progress =  ((double)snapshot.progress.completedUnitCount / (double)snapshot.progress.totalUnitCount);
+                                                      [self.bridge.eventDispatcher sendAppEventWithName:@"FirebaseUploadProgressChanged" body: @{ @"progress": [NSNumber numberWithDouble:progress], @"key": key}];
+                                                      
+                                                  }];
     
 }
 
